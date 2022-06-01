220eb1b4c97f75df41cc6aeca9387f5e242863d4
sciloaf <sciloaf@pm.me>
2022-06-02 00:09:53 +0200

    Added very basic install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+sudo mkdir -p /opt/milla/share/icons
+sudo cp -av ../build-milla-Desktop-Release/bin /opt/milla/
+sudo cp -av ../build-milla-Desktop-Release/share /opt/milla/
+sudo cp -av milla-view/milla*.png /opt/milla/share/icons/
+sudo cp 3rdparty/* /opt/milla/share/
+
+[ "z$1" = "zupdate" ] && exit 0
+
+cd /opt/milla/share
+sudo cat colors_a* > colors.7z
+sudo 7zr e colors.7z
+sudo rm -vf colors_a* colors.7z
+
+cd -
+mkdir -p ~/.local/share/applications
+cp -av milla-view/milla-image-viewer.desktop ~/.local/share/applications/
+update-desktop-database ~/.local/share/applications
